9e8e10b946bff54d1adb894f3b46aaf87cb37834
goo-gy <lkukh17@naver.com>
2022-11-28 23:29:31 +0900

    init

---
+++ src/main/java/googy/practice/ModelMapperPracticeApplication.java
@@ -0,0 +1,13 @@
+package googy.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelMapperPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelMapperPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/googy/practice/ModelMapperPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package googy.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelMapperPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
